c368ba38d72457fe5fa730c1e7d9001894e07f42
Mykola Kushnir <mykola.kushnir.825@gmail.com>
2024-01-01 05:14:54 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -9,20 +9,10 @@ npm run docs:build
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:boldak/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-
-git push -f https://github.com/StrivingToAdoniss/IM-22_lab_and_course_works master:gh-pages
-# if you use ssh use this instead:
-# git push -f git@github.com:StrivingToAdoniss/IM-22_lab_and_course_works.git master:gh-pages
+git push -f https://github.com/kushnirko/db-coursework master:gh-pages
 
 cd -
